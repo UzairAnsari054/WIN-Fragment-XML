6574465aa93d6a413a123b5d27c6bbe019ba0997
Uzair Ansari <124576222+UzairAnsari054@users.noreply.github.com>
2025-12-22 18:27:13 +0530

    feature(Navigation): navigate with safe args

--- app/src/main/java/com/example/winfragmentxml/fragments/ChatFragment.kt
+++ app/src/main/java/com/example/winfragmentxml/fragments/ChatFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 import com.example.winfragmentxml.R
 
 class ChatFragment : Fragment() {
@@ -15,5 +17,14 @@ class ChatFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_chat, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val chatButton = view.findViewById<Button>(R.id.chat_button)
+        chatButton.setOnClickListener {
+            findNavController().navigate(ChatFragmentDirections.actionChatFragmentToStatusFragment())
+        }
     }
 }
