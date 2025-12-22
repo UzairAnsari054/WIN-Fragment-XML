3e8ba587bb7f1e1e254f90018c0d835809da73b9
Uzair Ansari <124576222+UzairAnsari054@users.noreply.github.com>
2025-12-22 18:48:49 +0530

    feature(Navigation with Argument): navigate safe args with string argument

--- app/src/main/java/com/example/winfragmentxml/fragments/StatusFragment.kt
+++ app/src/main/java/com/example/winfragmentxml/fragments/StatusFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import com.example.winfragmentxml.R
 
 class StatusFragment : Fragment() {
@@ -14,5 +16,15 @@ class StatusFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_status, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val safeArgs: StatusFragmentArgs by navArgs()
+        val name = safeArgs.name
+
+        val nameTV = view.findViewById<TextView>(R.id.name_tv)
+        nameTV.text = name
     }
 }
