7df6736ae245def7f438a2817fdcef2105acb29c
Uzair Ansari <124576222+UzairAnsari054@users.noreply.github.com>
2025-12-23 16:42:18 +0530

    feature(bottomNav): implement bottom navigation

--- app/src/main/java/com/example/winfragmentxml/MainActivity.kt
+++ app/src/main/java/com/example/winfragmentxml/MainActivity.kt
@@ -5,6 +5,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavHost
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +20,11 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.container) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav_container)
+        bottomNav.setupWithNavController(navController)
     }
 }
